dd5650febd8cf53acee4cb07979bc24678b61d62
HenaMoon <engineering27@naver.com>
2022-09-24 22:49:37 +0900

    commit message

--- [SHS] V모델 기반 소프트웨어 개발/3. 코드/Unit Test/Test_Environment/UnitTest/UnitTest/Humidifier.cpp
+++ [SHS] V모델 기반 소프트웨어 개발/3. 코드/Unit Test/Test_Environment/UnitTest/UnitTest/Humidifier.cpp
@@ -44,14 +44,14 @@ int Mode_Controller(string mode_set, int error_signal) {
     // Manual Mode Controller_On  1.3.2
     if (mode_set == "1") {
         error_signal++;
-        return manual_mode_on(error_signal); // 수동모드 ON을 실행하기 위한 함수 
+        return manual_mode_on(error_signal); 
 
     }
 
     // Manual Mode Controller_OFF 1.3.2
     else if (mode_set == "2") {
         error_signal = 0;
-        return manual_mode_off(); // 수동모드 OFF를 실행하기 위한 함수
+        return manual_mode_off();
     }
 
     // Auto Mode Controller_ON 1.3.3
@@ -66,26 +66,26 @@ int Mode_Controller(string mode_set, int error_signal) {
     // Auto Mode Controller_OFF 1.3.3
     else if (mode_set == "5") {
         error_signal = 0;
-        return auto_mode_off(); // 자동모드 OFF를 실행하기 위한 함수
+        return auto_mode_off(); 
 
     }
     else return error_num;
 
 }
 
-int manual_mode_on(int error_signal) {     // 수동모드 기능을 ON 할때 실행
+int manual_mode_on(int error_signal) {     
     if (error_signal > 1) {
         return error_num;
     }
     return manual_on_succeed;
 }
 
-int manual_mode_off() {    // 수동모드 기능을 OFF 할때 실행
+int manual_mode_off() {   
 
     return manual_off_succeed;
 }
 
-int auto_mode_off() {    // 수동모드 기능을 OFF 할때 실행
+int auto_mode_off() {    
     return auto_off_succeed;
 }
 // Auto Mode Control Succeed 1.3.3.1, Auto Mode Control Fail 1.3.3.2     
@@ -104,27 +104,27 @@ int Auto_Controller(int auto_control, float h, float t, int error_signal) {
             return error_num;
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족 1.3.3.1 
-        else if (24 <= t <= 30 && 45 <= h) {           // t >= 32 && h >= 70
+        else if (24 <= t && 60 <= h) {          
             return auto_humi_succeed();
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족X 1.3.3.2
-        else if (24 <= t <= 30 && h <= 35) {       // t >= 32 && h <= 60
+        else if (24 <= t && h < 60) {       
             return  auto_humi_fail();
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족 1.3.3.1 
-        else if (21 <= t <= 23 && 55 <= h) { // 29 <= t <= 31 && h >= 65
+        else if (21 <= t <= 23 && 50 <= h) { 
             return auto_humi_succeed();
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족X 1.3.3.2
-        else if (21 <= t <= 23 && h <= 45) {  // 29 <= t <= 31 && h <= 55
+        else if (21 <= t <= 23 && h < 50) {  
             return auto_humi_fail();
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족 1.3.3.1 
-        else if (18 <= t <= 20 && 65 <= h) {  // 27 <= t <= 29 && h >= 55
+        else if (18 <= t <= 20 && 40 <= h) {  
             return auto_humi_succeed();
         }
         // 자동설정 켜진 상태에서 온도별 습도 만족X 1.3.3.2
-        else if (18 <= t <= 20 && h <= 55) {  // 27 <= t <= 29 && h <= 50
+        else if (18 <= t <= 20 && h < 40) {  
             return auto_humi_fail();
         }
     }
@@ -132,7 +132,7 @@ int Auto_Controller(int auto_control, float h, float t, int error_signal) {
 }
 
 
-int auto_humi_succeed() {        // 자동모드 기능에서 온도별 적정 습도를 벗어날 때 실행
+int auto_humi_succeed() {        
 
     return auto_on_succeed;
 
@@ -142,17 +142,17 @@ int auto_humi_fail() {
     return auto_on_fail;
 }
 
-int setting_mode_on(int error_signal) {     // 습도 설정모드 기능 사용시 설정습도가 현재 습도 보다 높을 때 실행
+int setting_mode_on(int error_signal) {     
     if (error_signal > 1) {
         return error_num;
     }
     return set_on_succeed;
 }
-int setting_mode_off() {   // 습도 설정모드 기능 사용시 설정습도가 현재습도보다 높게 셋팅 된 상태에서 현재습도가 설정습도값보다 클때
+int setting_mode_off() {   
     return set_off_succeed;
 
 }
-int setting_mistake() {    // 습도 설정을 0미만 또는 90초과로 설정시 실행
+int setting_mistake() {    
 
     return set_mistake;
 }
